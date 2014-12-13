8e322449fba1a4c83a0771447b01bd7a3343cb3a
Lin Chen <lchen00000@gmail.com>
2014-12-12 22:36:27 -0800

    josh’s game is done!

--- josh/game.js
+++ josh/game.js
@@ -2,10 +2,33 @@
 /*jslint sloppy:true, browser: true, devel: true, eqeq: true, vars: true, white: true*/
 var game;
 var highScore = 0;
+
+var menu = {
+    preload:function() {
+        game.load.image('bg', 'imgs/background.jpg');
+        game.load.image('tree','imgs/tree.png');
+        game.load.image('scroll', 'imgs/scroll.png');
+        game.load.image('start', 'imgs/start.png');
+        game.load.audio('jingleBells', 'music/jingleBells.mp3')
+    },
+    
+    create: function() {
+        this.bg = game.add.sprite(0,0, 'bg');
+        this.bg.width = game.world.width; this.bg.height = game.world.height;
+        this.tree = game.add.sprite(0, 90, 'tree');
+        this.scroll = game.add.sprite(200, 80, 'scroll');
+        this.scroll.scale.x = 0.74; this.scroll.scale.y  = 0.74;
+        this.start = game.add.button(440, 242, 'start', function() {game.state.start('main');}, this)
+        this.music = game.add.audio('jingleBells');
+        this.music.play('', 0, 1, true);
+ 
+    }
+};
+
 var flappyJosh = {
     preload: function() {
         game.load.image('bg', 'imgs/flappyBirdBackground.png');
-        game.load.image('josh', 'imgs/placeholder_mini.png');
+        game.load.image('josh', 'imgs/josh.png');
         game.load.image('pipe', 'imgs/pipe.png');
         game.load.image('pipe2', 'imgs/pipe2.png');
         game.load.image('board', 'imgs/board.png');
@@ -13,7 +36,7 @@ var flappyJosh = {
         game.load.image('back', 'imgs/backButton.png');
     },
     create: function() {
-        game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.gameOverhuh = false;
         
         //bg
         this.bg = game.add.sprite(0,0,'bg');
@@ -23,6 +46,7 @@ var flappyJosh = {
         
         //llama
         this.josh = game.add.sprite(0, game.world.height/2, 'josh');
+        this.josh.scale.x = 0.5; this.josh.scale.y = 0.5;
         game.physics.arcade.enable(this.josh);
         this.josh.body.gravity.y = 1000;
         
@@ -42,24 +66,20 @@ var flappyJosh = {
         this.pipes2.checkWorldBounds = true;
         this.pipes2.outOfBoundsKill = true;
         //texts
-        this.score = 0;  
-        this.scoreText = game.add.text(game.world.centerX, 0, ''+ this.score, { font: "30px Arial", fill: "#ffffff" });
+        this.score = -2;  
+        this.scoreText = game.add.text(game.world.centerX, 0, '', { font: "30px Arial", fill: "#ffffff" });
             
     },
     
     update: function() {
-        if (!this.josh.inWorld){
+        if (this.josh.inWorld == false){
             this.gameOver();
         }
-        //fix this
-        this.pipes.forEachAlive(function(pipeT) {
-            if (!pipeT.inWorld)
-                this.score++;
-        },this);
         
-        this.scoreText.setText(this.score);
         
-        game.physics.arcade.overlap(this.josh , this.pipes, this.gameOver, null, this);
+        this.scoreText.setText(Math.max(this.score, 0));
+        
+        game.physics.arcade.overlap(this.josh, this.pipes, this.gameOver, null, this);
         game.physics.arcade.overlap(this.josh, this.pipes2, this.gameOver, null, this); 
 
     },
@@ -69,6 +89,9 @@ var flappyJosh = {
     },
     
     gameOver: function() {
+        if (highScore<= this.score)
+            highScore = this.score;
+        this.gameOverhuh = true;
         this.josh.body.velocity.y = 0;
         this.pipes.forEachAlive(function(pipeT){
             pipeT.body.velocity.x = 0;
@@ -77,31 +100,35 @@ var flappyJosh = {
             pipeB.body.velocity.x = 0;
         },this);
         this.gameOverBoard = game.add.sprite(50, 50, 'board');
-        this.back = game.add.button(60, 350, 'back', function() {game.state.start('mgSelection')}, this);
-        this.back.visible = false;
-        this.replay = game.add.button(this.back.position.x + this.back.width/2, this.back.position.y,'replay', this.restart, this);
-        this.scoreText2 = game.add.text(game.world.centerX - 200,game.world.centerY - 100, 'Score:' + this.score, { font: "40px Arial", fill: "black" });
-        this.highScoreText = game.add.text(game.world.centerX - 200,game.world.centerY , 'High score:' + highScore, { font: "40px Arial", fill: "black" });
+        this.gameOverBoard.x = game.world.width/2 - this.gameOverBoard.width/2;
+        this.replay = game.add.button(0, 350,'replay', this.restart, this);
+        this.replay.x = game.world.width/2 - this.replay.width/2;
+        this.scoreText2 = game.add.text(game.world.centerX - 200,game.world.centerY - 100, 'Score:' + Math.max(this.score, 0), { font: "60px Arial", fill: "black" });
+        this.highScoreText = game.add.text(game.world.centerX - 200,game.world.centerY  , 'High score:' + highScore, { font: "60px Arial", fill: "black" });
     },
     
     restart: function() {
         game.state.start('main');
     },
     
-    addOnePipe: function(x, y) { 
-        var pipeT = this.pipes.getFirstDead();
-        pipeT.reset(game.world.width, Math.random() * -200);
-        pipeT.body.velocity.x = -200;
-        pipeT.checkWorldBounds = true;
-        pipeT.outOfBoundsKill = true;
+    addOnePipe: function(x, y) {
+        if (!this.gameOverhuh) {
         
-        
-        var pipeB = this.pipes2.getFirstDead();
-        pipeB.reset(pipeT.position.x, pipeT.position.y + 145  + pipeT.height);
-        pipeB.body.velocity.x = -200;
-        pipeB.checkWorldBounds = true;
-        pipeB.outOfBoundsKill = true;
-        
+            this.score++;
+            var pipeT = this.pipes.getFirstDead();
+            pipeT.reset(game.world.width, Math.random() * -200);
+            pipeT.body.velocity.x = -200;
+            pipeT.checkWorldBounds = true;
+            pipeT.outOfBoundsKill = true;
+
+
+            var pipeB = this.pipes2.getFirstDead();
+            pipeB.reset(pipeT.position.x, pipeT.position.y + 190 + pipeT.height);
+            pipeB.body.velocity.x = -200;
+            pipeB.checkWorldBounds = true;
+            pipeB.outOfBoundsKill = true;
+        } else
+            return;
     }
         
         
@@ -112,4 +139,5 @@ game = new Phaser.Game(640, 480, Phaser.AUTO, 'gameDiv');
 
 // And finally we tell Phaser to add and start our 'main' state
 game.state.add('main', flappyJosh);
-game.state.start('main');
+game.state.add('menu',menu);
+game.state.start('menu');
